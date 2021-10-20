974a6abb810a0e509b64491105eafde2e5518727
balram sahu <balramsahu8777@gmail.com>
2021-10-20 16:30:40 +0530

    graphql

--- graphql_nodejs_typeorm/backend/resolvers/user_resolver.ts
+++ graphql_nodejs_typeorm/backend/resolvers/user_resolver.ts
@@ -1,11 +1,7 @@
 import { Resolver, Query,Mutation,Arg } from "type-graphql";
 import { Users } from '../models/users';
-import { Product } from '../models/product';
-import { Category } from '../models/category';
 
 import {CreateUserInput} from '../inputs/create_user_input'
-import {CreateProductInput} from '../inputs/CreateProductInput'
-import {UpdateProductInput} from '../inputs/UpdateProductInput'
 import {UpdateUserInput} from '../inputs/UpdateUserInput'
 
 
@@ -55,70 +51,5 @@ export class UserResolver {
     await user.save();
     return user;
   }
-
-  // For Products
-    //Get All Product
-    
-    // @Query(() => [Product])
-    // allProducts() {
-    //   return Product.find();
-    // }
-  
-    //Get Product By id
-    @Query(() => Product)
-    product(@Arg("id") id: string) {
-      return Product.findOne({ where: { id } });
-    }
-  
-    //Add Product
-    @Mutation(() => Product)
-    async createProduct(@Arg("data") data: CreateProductInput) {
-      const product = Product.create(data);
-      await product.save();
-      return product;
-    }
-  
-    //Update Product By id
-    @Mutation(() => Product)
-    async updateProduct(@Arg("id") id: string, @Arg("data") data: UpdateProductInput) {
-      const product = await Product.findOne({ where: { id } });
-      if (!product) throw new Error("product not found!");
-      Object.assign(product, data);
-      await product.save();
-      return product;
-    }
-  
-    //Delete Product by id
-    @Mutation(() => Boolean)
-    async deleteProduct(@Arg("id") id: string) {
-      const product = await Product.findOne({ where: { id } });
-      if (!product) throw new Error("product not found!");
-      await product.remove();
-      return true;
-    }
-
-    //Showing data of product categories and main product
-  @Query(() => [Category])
-  async AllProducts() {
-    
-    // const products_categories= Category.find();
-    const products_categories = Category.find({
-      // where:{
-      //   active:true
-      // },
-      relations:['productParticipants']
-    });
-
-    /*let experiecesData = await this.Product.find({
-      where: {
-        id: value.pr_id
-      },
-      relations: ['productParticipants']
-    })*/
-    return products_categories;
-  }
-  // AllProduct(@Arg("id") id: string) {
-  //   return Products.findOne({ where: { id } });
-  // }
 
 }
